91b29229fcf713dd5f872846cdb471c2cc03b52b
Vansh Kalra <vanshkalra645@gmail.com>
2023-09-12 19:38:28 +0530

    Basic App before rendering component instead of direct HTML in App.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { nanoid } from 'nanoid'
+import Questions from './components/Questions';
+import Buttons from './components/Buttons';
 import './App.css';
 
+
+
 function App() {
+  const [playingGame, setPlayingGame] = React.useState(true)
+  const [quizData, setQuizData] = React.useState([]);
+  const [isHeld, setIsHeld] = React.useState();
+  const he = require("he")
+
+  // CORRECT ANSWERS ARRAY, TO MATCH CORRECT ANSWERS
+  const correctAnswers = [];
+  for (let i = 0; i < quizData.length; i++) {
+    correctAnswers.push(quizData[i].correct_answer)
+  }
+  console.log(correctAnswers)
+  // FETCHING DATA
+  const fetchQues = () => {
+    fetch("https://opentdb.com/api.php?amount=5&category=27&difficulty=easy&type=multiple")
+      .then(response => response.json())
+      .then(data => {
+        setQuizData(data.results);
+      });
+  };
+
+  React.useEffect(() => {
+    fetchQues();
+  }, []);
+  // Function to shuffle options
+  function shuffleOptions(quizItem) {
+    const options = [...quizItem.incorrect_answers, quizItem.correct_answer];
+    for (let i = options.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [options[i], options[j]] = [options[j], options[i]];
+    }
+    return options;
+  }
+
+  function buttonClicked(value) {
+    if (correctAnswers.includes(value)) {
+      alert("CORRECT ANSWER!")
+    } else {
+      alert("WRONG ANSWER!")
+    }
+  }
+  // MAIN RENDER RETURN
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // playingGame
+    //   ?
+    //   <div>
+    //     <p>Quizzical App</p>
+    //     <button onClick={() => setPlayingGame(false)}>Start Game</button>
+    //   </div>
+    //   :
+    <div className='main-container'>
+      {quizData.map((quizItem, index) => (
+        <div key={index}>
+          <div>
+            {/* FIXED &#039 instead of '  */}
+            <h2>{he.decode(quizItem.question)}</h2>
+          </div>
+          <div>
+            {shuffleOptions(quizItem).map((eachOpt, j) => (
+              <button onClick={() => buttonClicked(eachOpt)} className="btn btn-outline-dark" key={j}>{he.decode(eachOpt)}</button>
+            ))}
+          </div>
+          <hr></hr>
+        </div>
+      ))}
+      <button>Submit</button>
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/components/Questions.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Buttons from './Buttons';
+
+
+export default function Questions(props) {
+    
+   
+}
